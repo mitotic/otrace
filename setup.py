029ea08f92a01ca61cec239f2172bfcacada8e64
R. Saravanan <sarava@sarava.net>
2012-06-06 00:07:57 -0500

    Conditional install of ordereddict.py

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@
 
 from distutils.core import setup
 
+py_modules = ["basetrace", "otrace"]
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    py_modules.append("ordereddict")
+
 setup(name='otrace',
       version='0.30',
       description='otrace: An object-oriented tracing tool for nonlinear debugging',
       author='Ramalingam Saravanan',
       author_email='sarava@sarava.net',
       url='http://info.mindmeldr.com/code/otrace',
-      py_modules=['otrace'],
+      py_modules=py_modules,
      )
