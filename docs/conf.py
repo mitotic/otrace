d2992815158cd41fd7fe94fe3e019a28c1fae518
R. Saravanan <sarava@mindmeldr.com>
2012-10-06 23:56:17 -0500

    Typo fix

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ copyright = u'2012, R. Saravanan'
 # built documents.
 #
 # The short X.Y version.
-version = '0.10.0'
+version = '0.30.9'
 # The full version, including alpha/beta/rc tags.
-release = '0.10.0'
+release = '0.30.9'
 
 if about:
     version = about.version
